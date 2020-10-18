4651d020d223a09e147ac404fb39718b83496bb1
Ryan Nam <nam.changeworld@gmail.com>
2020-10-18 23:08:03 +0900

    If applied, this commit will create statistic of customer and booking room

---
+++ WebProject/Models/Statistics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelWebApp.Models
+{
+    public class Statistics
+    {
+        // - how many customersa are located in each postcode.
+        // - how many bookings have been made for each room.
+
+        [Display(Name = "Room ID")]
+        public int Room 
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Number of Bookings")]
+        public int BookingCount
+        {
+            get;
+            set;
+        }
+
+
+        [Display(Name = "Postcode")]
+        public String Postcode
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Number of Customers")]
+        public int CustomerCount;
+    }
+}
---
+++ WebProject/Pages/Bookings/HotelStatistics.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelWebApp.Pages.Bookings
+{
+    [Authorize(Roles = "Administrators")]
+    // Fristly, I made a CountStatistic.cs class model
+    // Secondly, Add the property _context && the class constructor below
+    // Add using --.Models; && IList to pass data to Content file
+    public class HotelStatisticsModel : PageModel
+    {
+        private readonly HotelWebApp.Data.ApplicationDbContext _context;
+
+        public HotelStatisticsModel(HotelWebApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // For passing the results to the Content file
+        // IList<ModelClass> name
+        // For passing the results to the Content file
+        public IList<Statistics> PostcodesStats;
+        public IList<Statistics> RoomStats;
+
+        // GET: Booking/HotelStatistics
+        // OnGET -> OnGetAsync
+        public async Task<PageResult> OnGetAsync()
+        {
+            // divide the Pizzas into groups by Pizza Count
+            var bookingGroup = _context.Booking.GroupBy(m => m.RoomID);
+
+            // for each group, get its count value and the number of pizzas in this group
+            RoomStats = await bookingGroup.Select(r => new Statistics
+            {
+                Room = r.Key,
+                BookingCount = r.Count()
+            }).ToListAsync();
+
+
+            var CustomerGroup = _context.Customer.GroupBy(c => c.Postcode);
+
+            PostcodesStats = await CustomerGroup.Select(c => new Statistics
+            {
+                Postcode = c.Key,
+                CustomerCount = c.Count()
+            }).ToListAsync();
+
+
+            return Page();
+
+            // so we will know which area, customer normally live and come here
+            // and which room is more popular.
+        }
+    }
+}
